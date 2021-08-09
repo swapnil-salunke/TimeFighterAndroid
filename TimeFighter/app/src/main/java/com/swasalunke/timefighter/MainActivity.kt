b704db1fef8c6d198cd1206f12cb8d45e9524c0f
swapnil-salunke <swapnil.salunke.mobile@gmail.com>
2021-08-09 09:47:42 +0530

    updating textfields

--- TimeFighter/app/src/main/java/com/swasalunke/timefighter/MainActivity.kt
+++ TimeFighter/app/src/main/java/com/swasalunke/timefighter/MainActivity.kt
@@ -2,10 +2,20 @@ package com.swasalunke.timefighter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var tapMeButton: Button
+    internal lateinit var gameScoreTextView: TextView
+    internal lateinit var timeLeftTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tapMeButton = findViewById(R.id.tapMeButton)
+        gameScoreTextView = findViewById(R.id.scoreTextView)
+        timeLeftTextView = findViewById(R.id.timeLeftTextView)
     }
 }
